61dcc325bfc628899dee7249d8d14d84e837473d
Горбаченко Олег <62697935+kykyshka69@users.noreply.github.com>
2022-03-16 22:45:57 +0200

    add button reset add header and fonts

--- script/app.js
+++ script/app.js
@@ -8,7 +8,11 @@ let kelvinColor = document.querySelector('.kelvin__tube--color');
 let celsiusColor = document.querySelector('.celsius__tube--color');
 let fahrenheitColor = document.querySelector('.fahrenheit__tube--color');
 
+// btn-reset value
+let btnReset = document.querySelector('.btn-reset')
 
+//btn-convert value
+let btnConvert = document.querySelector('.btn-conversion')
 
 //function change input value
 const handleChange = (e) => {
@@ -27,14 +31,11 @@ const handleChange = (e) => {
         default:
             break;
     }
-    
-    
-    fahrenheitColor.style.height = fahrenheit.value + 'px'
     //celsius
     if (celsius.value >= 500) {
         celsiusColor.style.height = 496 + 'px'  
     } else if (celsius.value <= 0) {
-         celsiusColor.style.height = 0 + 'px'
+         celsiusColor.style.height = Math.abs(celsius.value) + 'px'
     } else {
         celsiusColor.style.height = celsius.value + 'px'
     }
@@ -42,7 +43,7 @@ const handleChange = (e) => {
     if (kelvin.value >= 500) {
         kelvinColor.style.height = 496 + 'px'  
     } else if (kelvin.value <= 0) {
-         kelvinColor.style.height = 0 + 'px'
+         kelvinColor.style.height = Math.abc(kelvin.value) + 'px'
     } else {
         kelvinColor.style.height = kelvin.value + 'px'
     }
@@ -51,10 +52,25 @@ const handleChange = (e) => {
     if (fahrenheit.value >= 500) {
         fahrenheitColor.style.height = 496 + 'px'  
     } else if (fahrenheit.value <= 0) {
-         fahrenheitColor.style.height = 0 + 'px'
+         fahrenheitColor.style.height = Math.abs(fahrenheit.value) + 'px'
     } else {
         fahrenheitColor.style.height = fahrenheit.value + 'px'
     }
+
+    //btn-reset
+    btnReset.classList.remove('hide')
 }
 
+function setDefaultValue() {
+    kelvin.value = ''
+    celsius.value = ''
+    fahrenheit.value = ''
+    kelvinColor.style.height = 0 + 'px'
+    fahrenheitColor.style.height = 0 + 'px'
+    celsiusColor.style.height = 0 + 'px'
+    btnReset.classList.add('hide')
+
+}
+btnConvert.addEventListener('click', handleChange)
+btnReset.addEventListener('click', setDefaultValue)
 document.body.addEventListener('change', handleChange)
