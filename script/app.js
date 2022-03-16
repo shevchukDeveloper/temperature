2de5347b26585d21a3b77238f9217f5a12bda6e4
Горбаченко Олег <62697935+kykyshka69@users.noreply.github.com>
2022-03-16 17:25:08 +0200

    create basic html ,css style

---
+++ script/app.js
@@ -0,0 +1,4 @@
+let kelvin = document.querySelector('#input-kelvin');
+let celsius = document.querySelector('#input-celsius');
+let fahrenheit = document.querySelector('#input-fahrenheit');
+console.log(kelvin,celsius,fahrenheit);
